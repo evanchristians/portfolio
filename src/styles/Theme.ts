4f7cd59196cfd625ddf8c990f435e8d63829e598
evanchristians <evanryk@gmail.com>
2020-11-25 22:57:35 +0200

    color update

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,7 +3,7 @@ export const theme = {
     grey: "#2A2E32",
     greyLighter: "#eeeeee4f",
     greyLightest: "#666",
-    background: "#823DEF",
+    background: "#BE503E",
     mainDarker: "#C5445E",
     compliment: "#930077",
     primary: "#eee",
