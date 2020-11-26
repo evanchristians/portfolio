6b15708a3ab84ab4f8b41201628c5dd73a426d90
evanchristians <evanryk@gmail.com>
2020-11-26 14:28:47 +0200

    added generic hover state to resume link

--- src/components/ResumeLink.tsx
+++ src/components/ResumeLink.tsx
@@ -5,6 +5,8 @@ import { sleep } from "../lib/sleep";
 import { Theme } from "../types";
 
 const SResumeLink = styled.a`
+z-index: 1;
+  position: relative;
   display: flex;
   align-items: center;
   width: fit-content;
@@ -14,7 +16,31 @@ const SResumeLink = styled.a`
   color: ${({ theme }: { theme: Theme }) => theme.colors.white};
   border: 1px solid ${({ theme }: { theme: Theme }) => theme.colors.white};
   cursor: pointer;
-  transition: opacity 800ms ${({ theme }: { theme: Theme }) => theme.easings.outQuint};
+  transition: opacity 800ms
+    ${({ theme }: { theme: Theme }) => theme.easings.outQuint};
+
+  &:after {
+    z-index: -1;
+    content: "";
+    background: ${({ theme }: { theme: Theme }) => theme.colors.white};
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    transform: scaleX(0);
+    transform-origin: right;
+    transition: transform 400ms
+      ${({ theme }: { theme: Theme }) => theme.easings.outExpo};
+  }
+
+  &:hover {
+    color: ${({ theme }: { theme: Theme }) => theme.colors.background};
+    &:after {
+      transform: scaleX(1);
+      transform-origin: left;
+    }
+  }
 
   i {
     margin-left: 0.5rem;
