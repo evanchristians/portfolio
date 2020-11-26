03be96ed7743b3abaae534aa345a93f37737574d
evanchristians <evanryk@gmail.com>
2020-11-27 01:14:15 +0200

    added html lang

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from "next/document";
+import Document, { DocumentContext, Html } from "next/document";
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
 
@@ -19,11 +19,12 @@ export default class MyDocument extends Document {
         ...initialProps,
         styles: (
           <>
-            <head>
-              <title>work in progress...</title>
-              <meta
-                name="keywords"
-                content="
+            <Html lang="en-us">
+              <head>
+                <title>work in progress...</title>
+                <meta
+                  name="keywords"
+                  content="
                   resume,
                   portfolio,
                   evan,
@@ -46,20 +47,21 @@ export default class MyDocument extends Document {
                   town,
                   za
                 "
-              />
-              <meta
-                name="description"
-                content="Hi, my name is Evan Christians, I'm a Cape Town based software engineer with experience in full stack web development and strong interests in experience design and seamless integration."
-              />
-              <link rel="preconnect" href="https://fonts.gstatic.com" />
-              <link
-                href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Work+Sans:wght@900&display=swap"
-                rel="stylesheet"
-              />
-              <script src="https://kit.fontawesome.com/ce5d40ef27.js" />
-            </head>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
+                />
+                <meta
+                  name="description"
+                  content="Hi, my name is Evan Christians, I'm a Cape Town based software engineer with experience in full stack web development and strong interests in experience design and seamless integration."
+                />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                  href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Work+Sans:wght@900&display=swap"
+                  rel="stylesheet"
+                />
+                <script src="https://kit.fontawesome.com/ce5d40ef27.js" />
+              </head>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+            </Html>
           </>
         ),
       };
