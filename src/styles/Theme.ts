fa06e1dccb2a5ba23902555d0e34f7fc8b462280
evanchristians <evanryk@gmail.com>
2020-11-29 20:54:55 +0200

    trying some colors

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,7 +3,7 @@ export const theme = {
     grey: "#2A2E32",
     greyLighter: "#eeeeee4f",
     greyLightest: "#666",
-    background: "#BD3B1F",
+    background: "#7864F7",
     mainDarker: "#C5445E",
     compliment: "#930077",
     primary: "#eee",
