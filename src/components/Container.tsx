41ce7a0b5bcb60ebcc4aae29139a99f4e5b9d6e6
evanchristians <evanryk@gmail.com>
2021-04-04 16:28:18 +0200

    added locomotive scroll

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,16 +1,19 @@
+import { useLocomotiveScroll } from "react-locomotive-scroll";
 import styled from "styled-components";
 import { Theme } from "../types";
 
-const SContainer = styled.main`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  color: ${({ theme }: { theme: Theme }) => theme.colors.white};
-  background: ${({ theme }: { theme: Theme }) => theme.colors.background};
+const SContainer = styled.section`
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	color: ${({ theme }: { theme: Theme }) => theme.colors.white};
+	background: ${({ theme }: { theme: Theme }) => theme.colors.background};
 `;
 
 interface IContainer {}
 
 export const Container: React.FC<IContainer> = ({ children }) => {
-  return <SContainer>{children}</SContainer>;
+	const { scroll } = useLocomotiveScroll();
+	console.log(scroll);
+	return <SContainer data-scroll-container>{children}</SContainer>;
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,43 @@
 import { AppProps } from "next/app";
 import { useRouter } from "next/dist/client/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "../styles/GlobalStyle";
 import { theme } from "../styles/Theme";
 import * as gtag from "../lib/gtag";
 import GoogleFonts from "next-google-fonts";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
+	const router = useRouter();
+	const containerRef = useRef(null);
+	useEffect(() => {
+		const handleRouteChange = (url: URL) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;800&display=swap" />
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@800;900&display=swap" />
-      <GlobalStyle />
-      <Component {...pageProps}></Component>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<GoogleFonts href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;800&display=swap" />
+			<GoogleFonts href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@800;900&display=swap" />
+			<GlobalStyle />
+			<LocomotiveScrollProvider
+				options={{
+					smooth: true,
+				}}
+				watch={[]}
+				containerRef={containerRef}
+			>
+				<main data-scroll-container ref={containerRef}>
+					<Component {...pageProps}></Component>
+				</main>
+			</LocomotiveScrollProvider>
+		</ThemeProvider>
+	);
 };
 export default App;
